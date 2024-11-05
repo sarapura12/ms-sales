6bba7b829c017fbbfe10032e054552ab0514bc0c
PabloSarapura97 <rapid12300@gmail.com>
2024-11-05 20:10:20 -0300

    Add initial Spring Boot project setup

---
+++ ms-sales/src/main/java/com/salemanager/application/MsSalesApplication.java
@@ -0,0 +1,13 @@
+package com.salemanager.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSalesApplication.class, args);
+    }
+
+}
---
+++ ms-sales/src/test/java/com/salemanager/application/MsSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salemanager.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
