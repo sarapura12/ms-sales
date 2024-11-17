bba11fdb466a66be20b467663a76634c75e464f8
jonatan <jonatansalva.js14@gmail.com>
2024-11-17 13:28:34 -0300

    se agrego el logo al correo

--- sale-app/src/main/java/com/salemanager/application/service/impl/EmailServiceImpl.java
+++ sale-app/src/main/java/com/salemanager/application/service/impl/EmailServiceImpl.java
@@ -4,6 +4,7 @@ import com.salemanager.application.service.interfaces.IEmailService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,9 @@ public class EmailServiceImpl implements IEmailService {
 
     @Value("${spring.mail.username}")
     private String from;
+    @Value("${email.logo.path}")
+    private String logoPath;
+
 
     private final TemplateEngine templateEngine;
     private JavaMailSender mailSender;
@@ -29,13 +33,15 @@ public class EmailServiceImpl implements IEmailService {
     @Override
     public void sendMail(String to, String subject, Map<String, Object> variables) throws MessagingException {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
         helper.setSubject("Confirmación de compra");
 
         Context context = new Context();
         context.setVariables(variables);
         String htmlContent = templateEngine.process("purchaseMailTemplate", context);
         helper.setText(htmlContent, true);
+        ClassPathResource image = new ClassPathResource(logoPath);
+        helper.addInline("logo", image);
 
         helper.setTo(to);
         helper.setFrom(from);
